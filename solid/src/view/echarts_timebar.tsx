adbc5044878801ce6e81be870f1b4938a029bd3b
Greg Edwards <greg@greglearns.com>
2024-09-18 12:35:22 -0600

    DMB-847 feat: solid: timebar: add timestamp labels

--- solid/src/view/echarts_timebar.tsx
+++ solid/src/view/echarts_timebar.tsx
@@ -1,7 +1,8 @@
 import { EChartsAutoSize } from "echarts-solid";
 import { contextState } from "../context_state";
-import { mergeProps } from "solid-js";
-import { datazoomEventHandler } from "../state";
+import { mergeProps, Show } from "solid-js";
+import { datazoomEventHandler, getTimeAtPercentage } from "../state";
+import moment from "moment-timezone";
 
 interface IEchartsTimebarProps {
   class?: string;
@@ -9,6 +10,7 @@ interface IEchartsTimebarProps {
 
 export function EchartsTimebar(props: IEchartsTimebarProps) {
   const { state } = contextState();
+  const dateZero = +new Date();
   const base = {
     grid: {
       left: 0,
@@ -32,15 +34,38 @@ export function EchartsTimebar(props: IEchartsTimebarProps) {
     datazoom: datazoomEventHandler,
   };
 
+  const timezone = moment.tz.guess();
+  const timezoneAbbreviation = moment.tz(moment(), timezone).format("z");
+  const timeFormat = "HH:mm:ss [GMT]Z ";
+
+  const datasource = (): number[] => {
+    const timeEnd = state.server_now || dateZero;
+    const timeBegin = timeEnd - state.timeframe_ms;
+    const timeRef = { timeframe_ms: state.timeframe_ms, server_now: timeEnd };
+    const windowBegin = Math.floor(
+      getTimeAtPercentage(timeRef, state.range_begin),
+    );
+    return [
+      timeEnd - state.timeframe_ms,
+      windowBegin,
+      Math.max(
+        windowBegin + 1,
+        Math.ceil(getTimeAtPercentage(timeRef, state.range_end)),
+      ),
+      timeEnd,
+    ];
+  };
+
   return (
-    <div class={props.class}>
+    <div class={`relative ${props.class}`}>
       <EChartsAutoSize
         // @ts-expect-error eCharts types don't seem correct, so suppress TS error
         option={mergeProps(base, {
+          dataset: { source: datasource() },
           dataZoom: [
             {
-              show: true,
               type: "slider",
+              show: true,
               realtime: false,
               start: state.range_begin,
               end: state.range_end,
@@ -49,6 +74,49 @@ export function EchartsTimebar(props: IEchartsTimebarProps) {
         })}
         eventHandlers={eventHandlers}
       />
+      <section class="text-neutral-500">
+        <Show
+          when={
+            datasource()[0] !== datasource()[1] ||
+            datasource()[2] !== datasource()[3]
+          }
+        >
+          <p class="absolute isolate -z-10 -top-4 inset-x-0 text-sm px-1 flex bg-inherit">
+            <span
+              style={{ "margin-left": state.range_begin + "%" }}
+              class="absolute z-10 bg-zinc-100 dark:bg-zinc-900 p-0.5 rounded"
+            >
+              {moment(datasource()[1]).format(timeFormat).split(/ /)[0]}
+            </span>
+            <span
+              style={{
+                "margin-left": "calc(" + state.range_end + "% - 3rem)",
+                opacity:
+                  "calc( 2 * " +
+                  (state.range_end - state.range_begin) / 100 +
+                  " )",
+              }}
+              class="-z-10 bg-zinc-100 dark:bg-zinc-900 p-0.5 rounded"
+            >
+              {moment(datasource()[2]).format(timeFormat).split(/ /)[0]}
+            </span>
+          </p>
+        </Show>
+        <p class="absolute -z-10 top-4 inset-x-0 text-sm px-2.5 flex justify-between">
+          <span class="bg-zinc-100 dark:bg-zinc-900 p-0.5 rounded">
+            {moment(datasource()[0]).format(timeFormat).split(/ /)[0]}
+          </span>
+          <span class="bg-zinc-100 dark:bg-zinc-900 p-0.5 rounded">
+            {moment(datasource()[0]).format(timeFormat).split(/ /)[1] +
+              " (" +
+              timezoneAbbreviation +
+              ")"}
+          </span>
+          <span class="bg-zinc-100 dark:bg-zinc-900 p-0.5 rounded">
+            {moment(datasource()[3]).format(timeFormat).split(/ /)[0]}
+          </span>
+        </p>
+      </section>
     </div>
   );
 }
