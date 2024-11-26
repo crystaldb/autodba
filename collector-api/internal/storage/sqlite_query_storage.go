1f3c47612f02b7cb71b631e28cd42b264f9baf3e
Steams <radclifferobs@gmail.com>
2024-11-26 15:10:54 -0500

    add back storage code

--- collector-api/internal/storage/sqlite_query_storage.go
+++ collector-api/internal/storage/sqlite_query_storage.go
@@ -6,8 +6,20 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var QueryStore QueryStorage
+
 type SQLiteQueryStorage struct {
 	db *sql.DB
+}
+
+func InitQueryStorage(dbPath string) error {
+	storage, err := NewSQLiteQueryStorage(dbPath)
+	if err != nil {
+		return err
+	}
+
+	QueryStore = storage
+	return nil
 }
 
 func NewSQLiteQueryStorage(dbPath string) (*SQLiteQueryStorage, error) {
@@ -17,7 +29,46 @@ func NewSQLiteQueryStorage(dbPath string) (*SQLiteQueryStorage, error) {
 	}
 
 	storage := &SQLiteQueryStorage{db: db}
+	if err := storage.initTables(); err != nil {
+		return nil, err
+	}
+
 	return storage, nil
+}
+
+func (s *SQLiteQueryStorage) initTables() error {
+	_, err := s.db.Exec(`
+        CREATE TABLE IF NOT EXISTS queries (
+            fingerprint TEXT PRIMARY KEY,
+            query TEXT
+        );
+        CREATE TABLE IF NOT EXISTS full_queries (
+            fingerprint TEXT PRIMARY KEY,
+            full_query TEXT
+        );
+    `)
+	return err
+}
+
+func (s *SQLiteQueryStorage) StoreQuery(fingerprint, query, fullQuery string) error {
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("INSERT OR REPLACE INTO queries (fingerprint, query) VALUES (?, ?)", fingerprint, query)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = tx.Exec("INSERT OR REPLACE INTO full_queries (fingerprint, full_query) VALUES (?, ?)", fingerprint, fullQuery)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit()
 }
 
 func (s *SQLiteQueryStorage) GetQuery(fingerprint string) (string, error) {
