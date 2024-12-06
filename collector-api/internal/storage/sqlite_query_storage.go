4823b11b81ca99e645afccf91470cd1769356b04
Mohammad Dashti <mdashti@gmail.com>
2024-12-06 13:45:58 -0800

    DBA-362 Backfill improvements (#103)
    * Added safety checks in recording rules generator
    
    * Added batch processing.
    
    * Added log messages for timing.

--- collector-api/internal/storage/sqlite_query_storage.go
+++ collector-api/internal/storage/sqlite_query_storage.go
@@ -75,6 +75,42 @@ func (s *SQLiteQueryStorage) StoreQuery(fingerprint, query, fullQuery string, co
 	return tx.Commit()
 }
 
+func (s *SQLiteQueryStorage) StoreBatchQueries(queries []QueryRep) error {
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback() // Will be ignored if tx.Commit() is called
+
+	// Prepare statements for both tables
+	stmtQueries, err := tx.Prepare("INSERT OR REPLACE INTO queries (fingerprint, query, last_update) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmtQueries.Close()
+
+	stmtFullQueries, err := tx.Prepare("INSERT OR REPLACE INTO full_queries (fingerprint, full_query, last_update) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmtFullQueries.Close()
+
+	// Execute batch inserts
+	for _, q := range queries {
+		_, err = stmtQueries.Exec(q.Fingerprint, q.Query, q.CollectedAt)
+		if err != nil {
+			return err
+		}
+
+		_, err = stmtFullQueries.Exec(q.Fingerprint, q.QueryFull, q.CollectedAt)
+		if err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
+
 func (s *SQLiteQueryStorage) GetQuery(fingerprint string) (string, error) {
 	var query string
 	err := s.db.QueryRow("SELECT query FROM queries WHERE fingerprint = ?", fingerprint).Scan(&query)
